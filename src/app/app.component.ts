71051868753b382cae470aa9a06155827f8046a7
Adam Weeks <aoweeks@gmail.com>
2017-12-10 23:55:03 +0000

    Add IonicPage decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,15 @@ import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
-export class MyApp {
-  rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+export class MyApp {
+  rootPage:any = 'HomePage';
+  
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen:
+  SplashScreen) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+    statusBar.styleDefault();
+    splashScreen.hide();
     });
   }
 }
-
